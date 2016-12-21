6d676db8e4b94901fc57a3440e3be6c7e97536a1
Scott LaFave <slafave@frontlineed.com>
2016-12-21 12:49:59 -0500

    Initial tester

--- src/TestHost/Controllers/CustomersController.cs
+++ src/TestHost/Controllers/CustomersController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Medidata.ZipkinTracer.Core;
 using TestHost.Model;
 
 namespace TestHost.Controllers
@@ -20,6 +22,40 @@ namespace TestHost.Controllers
             customers.Add(new Customer() { Name = "Good customer", Address = "UK", Telephone = "9878757654" });
             customers.Add(new Customer() { Name = "Awesome customer", Address = "France", Telephone = "34546456" });
             return Ok<IList<Customer>>(customers);
+        }
+
+        [HttpGet]
+        [Route("ExternalCall")]
+        public async Task<IHttpActionResult> CallAnotherService()
+        {
+            var context = Request.GetOwinContext();
+            var config = Startup.GetZipkinConfig();
+
+            var zipkinClient = new ZipkinClient(config, context);
+
+            var url = "http://authenticationsvc.idm.local:80";
+            var requestUri = "/api/credentials/4552";
+            HttpResponseMessage result;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+
+                // start client trace
+                var span = zipkinClient.StartClientTrace(new Uri(client.BaseAddress, requestUri), "GET", zipkinClient.TraceProvider.GetNext());
+                
+                zipkinClient.Record(span, "This is a standard Annotation.");
+
+                result = await client.GetAsync(requestUri).ConfigureAwait(false);
+
+                //zipkinClient.RecordLocalComponent(span, "This is a Local Component Annotation");
+
+                // Record the total memory used after the call
+                zipkinClient.RecordBinary(span, "client.memory", GC.GetTotalMemory(false));
+
+                // end client trace
+                zipkinClient.EndClientTrace(span, (int)result.StatusCode);
+            }
+            return Ok(await result.Content.ReadAsStringAsync());
         }
     }
 }
--- src/TestHost/Startup.cs
+++ src/TestHost/Startup.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Medidata.ZipkinTracer.Core;
+using Medidata.ZipkinTracer.Core.Middlewares;
 using Owin;
 
 namespace TestHost
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder appBuilder)
+        public void Configuration(IAppBuilder app)
         {
-            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            app.UseZipkin(GetZipkinConfig());
+
+            var httpConfiguration = new HttpConfiguration();
             WebApiConfig.Register(httpConfiguration);
-            appBuilder.UseWebApi(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+        }
+
+        internal static ZipkinConfig GetZipkinConfig()
+        {
+            return new ZipkinConfig
+            {
+                Domain = r => new Uri("http://ScottL-SuperFly-ZipkinTestHost.idm.local"),
+                ZipkinBaseUri = new Uri("http://10.111.69.31:9411"),
+                SpanProcessorBatchSize = 10,
+                SampleRate = 0.5,
+                Create128BitTraceId = true,
+                Bypass = r => false,
+                ExcludedPathList = new[] { "/elmah", "/elmah.axd", "/startup", "/swagger" }
+            };
         }
     }
 }
