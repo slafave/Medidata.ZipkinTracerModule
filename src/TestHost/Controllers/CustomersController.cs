5f0ff1fff70c82d7eba525fba3d06a36830d6e5a
Scott LaFave <slafave@frontlineed.com>
2016-12-21 10:33:10 -0500

    Add our bare-bones host

---
+++ src/TestHost/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestHost.Model;
+
+namespace TestHost.Controllers
+{
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            IList<Customer> customers = new List<Customer>();
+            customers.Add(new Customer() { Name = "Nice customer", Address = "USA", Telephone = "123345456" });
+            customers.Add(new Customer() { Name = "Good customer", Address = "UK", Telephone = "9878757654" });
+            customers.Add(new Customer() { Name = "Awesome customer", Address = "France", Telephone = "34546456" });
+            return Ok<IList<Customer>>(customers);
+        }
+    }
+}
---
+++ src/TestHost/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace TestHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+    }
+}
